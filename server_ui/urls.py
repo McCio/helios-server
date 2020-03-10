876e69356ecb070079a686fe0fafa9755f5bd290
Marco Ciotola <848222@stud.unive.it>
2020-03-11 00:52:45 +0100

    Add and use names for server_ui urls

--- server_ui/urls.py
+++ server_ui/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from .views import home, about, docs, faq, privacy
 
 urlpatterns = [
-  path('', home),
-  path('about', about),
-  path('docs', docs),
-  path('faq', faq),
-  path('privacy', privacy),
+  path('', home, name="home"),
+  path('about', about, name="about"),
+  path('docs', docs, name="docs"),
+  path('faq', faq, name="faq"),
+  path('privacy', privacy, name="privacy"),
 ]
